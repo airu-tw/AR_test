585f72f652c2448f65f4eaa05a3573eb45d77231
airu-tw <48587249+airu-tw@users.noreply.github.com>
2019-05-31 15:47:53 +0800

    test

---
+++ js/AR_init.js
@@ -0,0 +1,9 @@
+
+AFRAME.registerComponent('fish', {
+    init: function() {
+        const aEntity = document.querySelector("#fish_models");
+        aEntity.setAttribute('obj-model',{
+          obj: #fish-obj,
+          mtl: #fish-mtl2});
+    }
+});  
